f455a67e95e9c093948313fd51c3999b95ddacef
bizzy604 <kevinyamatta50@gmail.com>
2024-10-07 03:43:43 -0700

    Automated commit

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -9,7 +9,7 @@
  */
 export default function getListStudentIds(array) {
 	if (array instanceof Array) {
-	return array.map((array) => array.id);
+		return array.map((a) => a.id);
 	}
 	return [];
 }
