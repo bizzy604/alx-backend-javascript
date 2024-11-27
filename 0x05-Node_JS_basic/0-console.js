a0fa97dafd5766bf9af782f3678c996f16777f91
root <kevinyamatta50@gmail.com>
2024-11-27 09:37:53 -0800

    NodeJs Basics

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+/**
+ * Task 0 Displays message on STDOUT.
+ * @param {String} msg Message to be displayed,
+ * should be string value,
+ * Example: Hello NodeJS
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
